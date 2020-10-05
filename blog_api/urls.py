20acf29e85989909c21639a3b7726e2e8ff2bd7f
Jay Womack <womackjw@hotmail.com>
2020-10-04 18:29:17 -0700

    initial commit

---
+++ blog_api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+# from .views import PostList, PostDetail
+
+app_name = 'blog_api'
+
+urlpatterns = [
+    # # shows an individual post
+    # path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
+    # # shows all of the posts
+    # path('', PostList.as_view(), name='listcreate'),
+]
